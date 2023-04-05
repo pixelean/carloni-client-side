50c2b2562bf22c0068417fc1a7430d9637e448d6
pixelean <anikh499@gmail.com>
2023-04-05 15:37:33 +0600

    testimonial modify

--- src/Components/Home/Testimonial/Testimonial.js
+++ src/Components/Home/Testimonial/Testimonial.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Testimonial.css";
+import quotaion from "../../../Asset/Page1/quotation.png";
 
 const Testimonial = () => {
   const people = [
@@ -9,7 +10,7 @@ const Testimonial = () => {
       picture:
         "https://fancytailwind.com/static/profile13-9abe52b1e8851f9e4afde349f2789d3e.jpg",
       testimony:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur aliquid, illo reiciendis voluptatum asperiores.",
+        "Rene is very professional, friendly, and easy to talk too. This was our first mortgage and he made sure we understood all the information, plus he got us the best rates. I have recommended a few of my friends to him now and they are over the moon with the deals he did for them. Great guy and I will continue to tell my friends to go with his company.",
     },
     {
       name: "Shanon Brown",
@@ -17,7 +18,7 @@ const Testimonial = () => {
       picture:
         "https://fancytailwind.com/static/profile16-3468a6740e58a45b951142454acb94d0.jpg",
       testimony:
-        "Amet ipsam nam architecto doloribus earum asperiores voluptates minus quibusdam culpa!",
+        "Rene is very professional, friendly, and easy to talk too. This was our first mortgage and he made sure we understood all the information, plus he got us the best rates. I have recommended a few of my friends to him now and they are over the moon with the deals he did for them. Great guy and I will continue to tell my friends to go with his company.",
     },
   ];
   const [current, setCurrent] = useState(0);
@@ -40,27 +41,33 @@ const Testimonial = () => {
           </div>
 
           {/* :TESTIMONY CONTAINER */}
-          <div className="contentTestimonial z-10 relative py-10  pr-2 w-full md:w-3/5 min-h-360px rounded md:rounded-r-none  bg-opacity-70 md:bg-opacity-100 text-white">
+          <div className="contentTestimonial z-10 relative py-10   w-full md:w-3/5 min-h-360px rounded md:rounded-r-none  bg-opacity-70 md:bg-opacity-100 text-white">
             <div className="mx-auto max-w-md flex flex-col">
-              <h1 className="text-white text-4xl font-bold ">Happy customers say about us</h1>
+              <h1 className="text-white text-4xl font-bold ">
+                Happy <span className="underlineArrow"> customers </span> say
+                about us
+              </h1>
 
               {/* ::Quote SVG */}
               <div className="flex-shrink-0">
-                <svg
+                {/* <svg
                   className="w-10 lg:w-12 h-10 lg:h-12 my-8"
                   fill="currentColor"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                 >
                   <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
-                </svg>
+                </svg> */}
+                <img src={quotaion} alt="" className="my-10 quotaionImg"></img>
+
+                
               </div>
               {/* ::Testimony */}
               <p className="text-xl sm:text-1xl lg:text-2xl  leading-normal">
                 {people[current].testimony}
               </p>
               {/* ::Name */}
-              <h3 className="mt-2 text-base font-bold">
+              <h3 className="mt-5 text-lg text-base font-semibold">
                 {people[current].name}
               </h3>
               {/* ::Role */}
