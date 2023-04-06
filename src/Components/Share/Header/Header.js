5e8fb2ee4b01afce36dad3b06c38afd00752f54a
pixelean <anikh499@gmail.com>
2023-04-06 17:57:12 +0600

    home page testimonial issue resolve

--- src/Components/Share/Header/Header.js
+++ src/Components/Share/Header/Header.js
@@ -54,7 +54,7 @@ const Header = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu-compact dropdown-content  mt-3 p-2 shadow bg-base-100 rounded-box w-52 "
+              className=" menu-compact dropdown-content  mt-3 p-2 shadow bg-base-100 rounded-box w-52 "
             >
               {menuItems}
             </ul>
